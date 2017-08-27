aca729ee27e7e6ce55ceb300b76fda5840cec1e0
Mark Philip Dawal <radical.mark@gmail.com>
2017-08-28 00:50:18 +0400

    TODO: filter books to compare it with onshelf books then display the book from the shelf if matches

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -8,7 +8,8 @@ import Book from './Book'
 class ListBooks extends Component {
 	state = {
 		showResult: '',
-		books: []
+		books: [],
+		book: {}
 	}
 
 	isSearchQueryEmpty = (bool) => {
@@ -16,8 +17,14 @@ class ListBooks extends Component {
 		console.log(this.state.showResult)
 	}
 
-	componentDidMount() {
-		this.props.books.map((book) => this.props.onshelf.filter((a) => a.id === book.id ? <Book history={this.props.history} key={a.industryIdentifiers[0].identifier} cover={a.imageLinks.thumbnail} title={a.title} authors={a.authors} id={a.id} shelf={a.shelf} book={a} updateShelf={this.props.updateShelf} /> : <Book history={this.props.history} key={book.industryIdentifiers[0].identifier} cover={book.imageLinks.thumbnail} title={book.title} authors={book.authors} id={book.id} shelf={book.shelf} book={book} updateShelf={this.props.updateShelf} /> ))
+	// componentDidMount() {
+	// 	// this.props.books.map((book) => )
+	// 	this.props.results.map((book) => this.props.onshelf.filter((a) => a.id === book.id ? console.log(a) : console.log(book) ))
+	// }
+
+	bookLoader = (book) => {
+		let index = this.props.onshelf.indexOf(book)
+		index >= 0 ? this.setState({ book: this.props.onshelf(index)}) : this.setState({ book }) 
 	}
 
 	render() {
@@ -28,7 +35,8 @@ class ListBooks extends Component {
 			    <ol className="books-grid">
 					{this.state.showResult && (
 						this.props.results.map((book) => (
-							console.log(book)	 	
+							this.bookLoader(book),
+							<Book history={this.props.history} key={this.state.book.industryIdentifiers[0].identifier} cover={this.state.book.imageLinks.thumbnail} title={this.state.book.title} authors={this.state.book.authors} id={this.state.book.id} shelf={this.state.book.shelf} book={this.state.book} updateShelf={this.props.updateShelf} />
 						))
 					)}
 			    </ol>
